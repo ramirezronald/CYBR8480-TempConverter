ad0e872355dc3980d620df4855d09836a3c8b724
Ronald Ramirez <ronaldramirez@unomaha.edu>
2019-03-03 17:19:37 -0600

    all

---
+++ app/src/main/java/com/example/tempconverter/ConverterUtil.java
@@ -0,0 +1,14 @@
+package com.example.tempconverter;
+
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
